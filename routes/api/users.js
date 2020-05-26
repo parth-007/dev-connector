f055a20587b0d3317032087e3b88ba88fb446780
parth-007 <parthmangukiya77@gmail.com>
2020-05-27 00:24:01 +0530

    JWT done

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {
     check,
     validationResult
@@ -67,8 +69,22 @@ router.post('/', [
         await user.save();
 
         // Return the jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+        jwt.sign(payload,
+            config.get('jwtSecret'), {
+                expiresIn: 360000 // Optional but recommended
+            }, (err, token) => {
+                if (err) throw err;
+                res.json({
+                    token
+                });
+            });
 
-        res.send('User registered!');
+        // res.send('User registered!');
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error..!');
