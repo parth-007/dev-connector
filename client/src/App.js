9a3ad2cb6ce9d7078c05b599ccee4f362fba401a
parth-007 <parthmangukiya77@gmail.com>
2020-06-04 01:44:29 +0530

    Authentication using setAuthToken

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
 // Redux
@@ -12,8 +14,15 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 // Main App component
 const App = () => {
+  // useEffect is a React hook, [] will make sure it run only once, it is like componentDidMount
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
     <Provider store={store}>
       <Router>
