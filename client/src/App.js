30318c13f9c1e3861950f9f570a875f50dee64b2
parth-007 <parthmangukiya77@gmail.com>
2020-06-05 01:22:08 +0530

    PrivateRoute for Dashboard added, checked for forced requrest for dashboard, Profile reducer and get current profile of user in state

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+
 import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
@@ -34,6 +37,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register/" component={Register} />
               <Route exact path="/login/" component={Login} />
+              <PrivateRoute exact path="/dashboard/" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,7 +3,10 @@ import { combineReducers } from 'redux';
 // We will create 4 different reducers and importing them here
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
+
 export default combineReducers({
   alert,
-  auth
+  auth,
+  profile
 });
