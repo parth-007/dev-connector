6e0097d7c05c328c193a47d4eb3c1da3cd1bcecb
parth-007 <parthmangukiya77@gmail.com>
2020-06-06 03:18:59 +0530

    Edit Profile Added with Alert and state management with Dashboard-actions as dumb component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import Alert from './components/layout/Alert';
@@ -43,6 +44,11 @@ const App = () => {
                 exact
                 path="/create-profile"
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
               />
             </Switch>
           </section>
