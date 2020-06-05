2daf684d84c4f725d52a5d5d14d04d9354353872
parth-007 <parthmangukiya77@gmail.com>
2020-06-06 02:24:52 +0530

    Form added for create-profile and onChange and onSubmit added

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import Alert from './components/layout/Alert';
@@ -38,6 +39,11 @@ const App = () => {
               <Route exact path="/register/" component={Register} />
               <Route exact path="/login/" component={Login} />
               <PrivateRoute exact path="/dashboard/" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
