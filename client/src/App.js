9932db4df47cbd8af004d10cee72c22124cd750d
parth-007 <parthmangukiya77@gmail.com>
2020-06-09 02:30:53 +0530

    Add Experience Added

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import Alert from './components/layout/Alert';
@@ -49,6 +50,12 @@ const App = () => {
                 exact
                 path="/edit-profile"
                 component={EditProfile}
+              />
+
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
               />
             </Switch>
           </section>
