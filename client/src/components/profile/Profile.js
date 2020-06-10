b8a0c1674e863b8f525b528634383ec3e8e4da5d
parth-007 <parthmangukiya77@gmail.com>
2020-06-11 02:04:40 +0530

    Github Profiles added

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({
   getProfileById,
@@ -71,6 +72,10 @@ const Profile = ({
                 <h4> No Education credentials.</h4>
               )}
             </div>
+
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
