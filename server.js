50b4e2fac95395ffe062b8fb52c5824eb808dbcf
parth-007 <parthmangukiya77@gmail.com>
2020-07-04 21:53:28 +0530

    prep for deployment to heroku

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 // Connect database
 connectDB();
 
 // Init middleware
-app.use(express.json({
+app.use(
+  express.json({
     extended: false
-}));
-
-app.get('/', (req, res) => res.send('API running'));
-
+  })
+);
 
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -20,8 +19,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
